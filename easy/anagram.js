b28b0c6a74beb2df311babc7b6ddb5fd17bb69aa
Sarthak <sarthakbhatt@live.com>
2024-02-21 00:10:33 +0530

    fixed problem with anagram solution

--- easy/anagram.js
+++ easy/anagram.js
@@ -8,16 +8,17 @@ function isAnagram(str1, str2) {
   if (str1.length != str2.length) {
     return false;
   }
+
   let finalstr = "";
   for (let i = 0; i < str1.length; i++) {
     for (let j = 0; j < str2.length; j++) {
-      if (str1[i] == str2[j]) {
-        finalstr = finalstr + str1[i];
+      if (str1[i].toLowerCase() == str2[j].toLowerCase()) {
+        finalstr = finalstr + str1[i].toLowerCase();
         break;
       }
     }
   }
-  if (finalstr == str1) {
+  if (finalstr == str1.toLowerCase()) {
     return true;
   } else return false;
 }

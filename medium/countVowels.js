037bd0cd9e9e7b42e0addf08b7a6e1ed5aa162b6
Sarthak <sarthakbhatt@live.com>
2024-02-21 00:57:31 +0530

    countVowels solotion added

--- medium/countVowels.js
+++ medium/countVowels.js
@@ -6,7 +6,26 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let count = 0;
+  let str2 = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (str2[i] == "a") {
+      count = count + 1;
+    }
+    if (str2[i] == "e") {
+      count = count + 1;
+    }
+    if (str2[i] == "i") {
+      count = count + 1;
+    }
+    if (str2[i] == "o") {
+      count = count + 1;
+    }
+    if (str2[i] == "u") {
+      count = count + 1;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
